b561ed42f797d50d81ca33760a7a22a2a9874d39
unknown <zubairmatthee262@gmail.com>
2023-11-14 10:42:02 +0200

    Complete

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -60,7 +60,7 @@
     <div class="col-md-3 text-center">
         <ul class="list-unstyled mb-0">
             <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                <p>Cape Town , Matroosfontein</p>
+                <p> Matroosfontein</p>
             </li>
 
             <li><i class="fas fa-phone mt-4 fa-2x"></i>
